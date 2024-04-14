944740207edec85ad5b6fea4eeb963a6adf3709d
charleysmithschofield <charleysmithschofield@gmail.com>
2024-04-14 16:36:20 -0700

    add 02_seed file

--- 1_queries/propertyListingsByCity.sql
+++ 1_queries/propertyListingsByCity.sql
@@ -18,11 +18,11 @@
 -- Limit the number of results to 10.
 -- Only show listings that have a rating >= 4 stars.
 
-SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
-FROM properties
-LEFT JOIN property_reviews ON properties.id = property_id
-WHERE city LIKE '%ancouve%'
-GROUP BY properties.id
-HAVING avg(property_reviews.rating) >= 4
-ORDER BY cost_per_night
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
 LIMIT 10;
