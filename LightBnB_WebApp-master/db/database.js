d4bb2bfecb7c10cf4979a834b1cc5766b7506bcf
charleysmithschofield <charleysmithschofield@gmail.com>
2024-04-15 20:45:29 -0700

    refactor getAllReservations to pull information from the lightbnb database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -78,7 +78,24 @@ const addUser = function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = `
+    SELECT reservations.*, properties.*, users.*, avg(property_reviews.rating) AS average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN users ON reservations.guest_id = users.id
+    LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id, users.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+  `;
+  return pool.query(query, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 
